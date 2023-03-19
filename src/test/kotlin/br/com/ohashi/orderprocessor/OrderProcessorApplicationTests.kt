82c83c9b6d776b4d779812d2fcd091a8beb98050
veronica ohashi <veronica.ohashi@outlook.com>
2023-03-19 17:38:27 -0300

    initial commit

---
+++ src/test/kotlin/br/com/ohashi/orderprocessor/OrderProcessorApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ohashi.orderprocessor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderProcessorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
